c2bdd6f9940fa62befd6a1473c1e6fc9de27a223
wonbin <wonbin3977@gmail.com>
2025-10-29 15:48:52 +0900

    feat(PurchaseInput) : 사용자로부터 금액 입력받는 기능 구현

---
+++ src/main/java/lotto/input/PurchaseInput.java
@@ -0,0 +1,10 @@
+package lotto.input;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class PurchaseInput {
+    public static void purchaseInputHadnler() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = readLine();
+    }
+}
