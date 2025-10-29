2b20f36221ecd8aebe484beaa091e682f9cd698b
wonbin <wonbin3977@gmail.com>
2025-10-29 16:32:26 +0900

    refactor(purchaseInputValidator) : 숫자가 입력된지 확인하는 메서드 수정

--- src/main/java/lotto/input/PurchaseInput.java
+++ src/main/java/lotto/input/PurchaseInput.java
@@ -6,14 +6,15 @@ import lotto.validator.PurchaseInputValidator;
 
 public class PurchaseInput {
     static PurchaseInputValidator purchseValidator = new PurchaseInputValidator();
+    static Long purchaseAmount;
 
     public static void purchaseInputHadnler() {
         while (true) {
             System.out.println("구입금액을 입력해 주세요.");
             String input = readLine().trim();
             try {
-                purchseValidator.isBlank(input);
-                purchseValidator.isNumber(input.trim());
+                purchseValidator.checkBlank(input);
+                purchseValidator.checkNumber(input);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] " + e.getMessage());
--- src/main/java/lotto/validator/PurchaseInputValidator.java
+++ src/main/java/lotto/validator/PurchaseInputValidator.java
@@ -2,17 +2,16 @@ package lotto.validator;
 
 
 public class PurchaseInputValidator {
-    public void isBlank(String input) {
+    public void checkBlank(String input) {
         if (input.equals("")) {
             throw new IllegalArgumentException("입력값이 비어있습니다.");
         }
     }
 
-    public void isNumber(String input) {
-        try {
-            Long.parseLong(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해야합니다.");
+    public void checkNumber(String input) {
+        if (!input.matches("-?\\d+")) {
+            throw new IllegalArgumentException("정수를 입력해야 합니다.");
         }
     }
+
 }
