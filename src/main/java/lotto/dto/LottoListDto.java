422704e9ca1c465a6ddc3dd99ab90c99e1fa6ee9
wonbin <wonbin3977@gmail.com>
2025-10-30 12:47:44 +0900

    refactor(LottoListDto) : LottoList를 전달하는 DTO 클래스 생성

---
+++ src/main/java/lotto/dto/LottoListDto.java
@@ -0,0 +1,16 @@
+package lotto.dto;
+
+import java.util.List;
+import lotto.Lotto;
+
+public class LottoListDto {
+    private final List<Lotto> lottolist;
+
+    public LottoListDto(List<Lotto> lottolist) {
+        this.lottolist = lottolist;
+    }
+
+    public List<Lotto> getLottolist() {
+        return lottolist;
+    }
+}
