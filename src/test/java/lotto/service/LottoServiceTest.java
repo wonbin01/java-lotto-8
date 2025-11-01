e6d9b76e3dfd5584dcd2a4d41e14914ff09fa312
wonbin <wonbin3977@gmail.com>
2025-11-01 23:42:24 +0900

    test(LottoServiceTest) : 로또 당첨 결과 검증하는 테스트 작성

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,7 +1,12 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMatchResult;
+import lotto.dto.BonusNumberDto;
 import lotto.dto.LottoListDto;
 import lotto.dto.PurchaseDto;
+import lotto.dto.WinningNumbersDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +18,30 @@ public class LottoServiceTest {
         LottoListDto lottoListDto = service.generateLottos(dto);
 
         Assertions.assertThat(lottoListDto.getLottolist()).hasSize(5);
+    }
 
+    @Test
+    public void 당첨_결과_정상_검증() {
+        LottoService service = new LottoService();
+
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6)); //6개 맞음
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7)); //5개 맞음 + 보너스
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 8)); //5개 맞음 + 보너스 x
+        LottoListDto lottoListDto = new LottoListDto(List.of(lotto1, lotto2, lotto3));
+        WinningNumbersDto winningNumbersDto = new WinningNumbersDto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumberDto bonusNumberDto = new BonusNumberDto(7);
+
+        List<LottoMatchResult> results = service.validateResult(lottoListDto, winningNumbersDto,
+                bonusNumberDto);
+        Assertions.assertThat(results);
+
+        Assertions.assertThat(results.get(0).getMatchCount()).isEqualTo(6);
+        Assertions.assertThat(results.get(0).getIsBonusMatch()).isFalse();
+
+        Assertions.assertThat(results.get(1).getMatchCount()).isEqualTo(5);
+        Assertions.assertThat(results.get(1).getIsBonusMatch()).isTrue();
+
+        Assertions.assertThat(results.get(2).getMatchCount()).isEqualTo(5);
+        Assertions.assertThat(results.get(2).getIsBonusMatch()).isFalse();
     }
 }
